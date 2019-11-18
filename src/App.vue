333aa1cde3632dd0c4b45a51ca29a18d04d427be
root <root@TheBestNew01-PC.localdomain>
2019-11-18 09:39:34 +0100

    initialize repo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="blue" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <exerciseCard :exerciseData="exampleData" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ExerciseCard from "./components/ExerciseCard";
 
 export default {
-  name: 'app',
+  props: {
+    source: String
+  },
   components: {
-    HelloWorld
-  }
-}
+    ExerciseCard
+  },
+  data: () => ({
+    drawer: null,
+    exampleData: [
+      {
+        date: "15.11.2019",
+        category: "Pushups",
+        warmupReps: "2",
+        warmupExercise: "Close-Pushups",
+        workReps: "2",
+        workExercise: "One-Arm-Pushups"
+      },
+      {
+        date: "17.11.2019",
+        category: "Pullups",
+        warmupReps: "2",
+        warmupExercise: "Close-Pullups",
+        workReps: "2",
+        workExercise: "One-Arm-Pullup"
+      }
+    ]
+  })
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ExerciseCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fluid>
+    <v-row class="grey lighten-5">
+      <v-card
+        class="ma-3 pa-6 transparent"
+        width="300"
+        height="300"
+        elevation="30"
+        raised
+        v-for="(card, index) in exerciseData"
+        :key="index"
+      >
+        <div class="bg"></div>
+        <v-list-item>
+          <v-list-item-content>
+            <div class="overline">
+              <v-icon>mdi-calendar</v-icon>
+              {{card.date}}
+            </div>
+          </v-list-item-content>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-card-title>
+          <v-icon large left>mdi-arm-flex</v-icon>
+        </v-card-title>
+        <v-card-title>{{ card.warmupReps }}x {{ card.warmupExercise }}</v-card-title>
+        <v-card-title>{{ card.workReps }}x {{ card.workExercise }}</v-card-title>
+        <v-divider></v-divider>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ExerciseCard",
+  props: ["exerciseData"],
+  data: () => ({})
+};
+</script>
+
+<style>
+.transparent {
+  z-index: 5;
+}
+
+.transparent .bg {
+  background: url("../assets/push-up.png") center center;
+  opacity: 0.4;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+}
+</style>
